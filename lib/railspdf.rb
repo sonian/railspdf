47824a243c7a18097424512073af3b9c601e61ae
Ubiratan Pires Alberton <u.alberton@gmail.com>
2009-10-12 17:57:21 -0300

    Removed requires to avoid config.gem problems.

--- lib/railspdf.rb
+++ lib/railspdf.rb
@@ -1,9 +1,8 @@
-require 'pdf/writer'
-require 'pdf/simpletable'
+require 'pdf/simpletable' if defined? PDF::Writer
 
 module RailsPDF
-  
- 	#this code comes from http://wiki.rubyonrails.com/rails/pages/HowtoGeneratePDFs 	
+
+ 	#this code comes from http://wiki.rubyonrails.com/rails/pages/HowtoGeneratePDFs
 	class PDFRender < ActionView::Base
   	include ApplicationHelper
 
@@ -22,12 +21,12 @@ module RailsPDF
     end
 
     def render(template, local_assigns = {})
-    	
-    	#get the instance variables setup	    	
+
+    	#get the instance variables setup
    		@action_view.controller.instance_variables.each do |v|
         instance_variable_set(v, @action_view.controller.instance_variable_get(v))
       end
-		
+
 			#keep ie happy
 			if @action_view.controller.request.env['HTTP_USER_AGENT'] =~ /msie/i
         @action_view.controller.headers['Pragma'] ||= ''
@@ -36,7 +35,7 @@ module RailsPDF
         @action_view.controller.headers['Pragma'] ||= 'no-cache'
         @action_view.controller.headers['Cache-Control'] ||= 'no-cache, must-revalidate'
    		end
-   		
+
    		@action_view.controller.headers['Content-Type'] = 'application/pdf'
 			if @rails_pdf_name
 				@action_view.controller.headers['Content-Disposition'] = 'attachment; filename="' + @rails_pdf_name + '"'
@@ -46,18 +45,18 @@ module RailsPDF
 				 #since we weren't provided a custom name
 				@action_view.controller.headers['Content-Disposition'] = 'attachment; filename="' + @action_view.controller.controller_name + '.pdf' + '"'
 			end
-      
+
       if @landscape
      		pdf = PDF::Writer.new( :paper => (@paper || 'A4'), :orientation => :landscape )
       else
      		pdf = PDF::Writer.new( :paper => (@paper || 'A4') )
       end
  	  	pdf.compressed = true if RAILS_ENV != 'development'
- 	  	
+
 	    eval template.source, nil
    		pdf.render
   	end
-  	
+
   	def self.call(template)
       "RailsPDF::PDFRender.new(self).render(template, local_assigns)"
     end
